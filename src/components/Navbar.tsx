3571ffedeca5087c92dfa24b803c18b546265282
baranecp <baranec.dev@gmail.com>
2025-09-23 13:17:59 +0200

    Refactor Navbar component to use navItems array for rendering menu items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,28 +5,34 @@ import BudgetsIcon from "../../public/budgets.svg";
 import PotsIcon from "../../public/pots.svg";
 import TransactionsIcon from "../../public/transactions.svg";
 
+const navItems = [
+  { icon: OverviewIcon, label: "Overview" },
+  { icon: TransactionsIcon, label: "Transactions" },
+  { icon: BudgetsIcon, label: "Budgets" },
+  { icon: PotsIcon, label: "Pots" },
+  { icon: BillsIcon, label: "Bills" },
+];
+
 export default function Navbar() {
   return (
-    <section className='container bg-grey-900 fixed bottom-0 left-0 rounded-t-lg overflow-hidden'>
-      <nav>
-        <ul className='flex justify-between px-4 pt-2'>
-          <li className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg antialiased transition-colors duration-150 cursor-pointer'>
-            <OverviewIcon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
-          </li>
-          <li className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg transition-colors duration-150 cursor-pointer'>
-            <TransactionsIcon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
-          </li>
-          <li className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg transition-colors duration-150 cursor-pointer'>
-            <BudgetsIcon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
-          </li>
-          <li className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg transition-colors duration-150 cursor-pointer'>
-            <PotsIcon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
-          </li>
-          <li className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg transition-colors duration-150 cursor-pointer'>
-            <BillsIcon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
-          </li>
-        </ul>
-      </nav>
-    </section>
+    <>
+      {/* === Mobile Navbar === */}
+      <section className='container bg-grey-900 fixed bottom-0 left-0 rounded-t-lg overflow-hidden md:hidden'>
+        <nav>
+          <ul className='flex justify-between px-4 pt-2'>
+            {navItems.map((item, idx) => {
+              const Icon = item.icon;
+              return (
+                <li
+                  key={idx}
+                  className='group px-6 py-3 self-center border-b-4 border-transparent hover:bg-beige-100 hover:border-secondary-green rounded-t-lg transition-colors duration-150 cursor-pointer'>
+                  <Icon className='w-auto h-auto text-grey-300 group-hover:text-secondary-green transition-colors' />
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </section>
+    </>
   );
 }
