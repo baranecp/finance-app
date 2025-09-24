75c5a984af3c79a4d03a0e60042cba30f2ba0c3a
baranecp <baranec.dev@gmail.com>
2025-09-24 12:38:27 +0200

    Fix padding issue in mobile screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
 // import TransactionList from "@/components/TransactionList";
 "use client";
-import Navbar from "@/components/Navbar/Navbar";
-import { motion } from "framer-motion";
-import "../styles/globals.css";
 import { Public_Sans } from "next/font/google";
-import Overview from "@/components/Overview";
 import { useSidebarStore } from "@/store/useSidebarStore";
+import { motion } from "framer-motion";
+import { useMediaQuery } from "../hooks/useMediaQuery";
+import Navbar from "@/components/Navbar/Navbar";
+import Overview from "@/components/Overview";
+import "../styles/globals.css";
 
 const PublicSans = Public_Sans({
   weight: ["400", "700"],
 });
 
 export default function Home() {
+  const isLargeScreen = useMediaQuery("(min-width: 1024px)");
   const { isOpen } = useSidebarStore();
   return (
     <div className='lg:flex lg:h-screen'>
       <Navbar />
       <motion.main
-        animate={{ marginLeft: isOpen ? 40 : 40 }}
+        animate={{ marginLeft: isLargeScreen ? (isOpen ? 40 : 40) : 0 }}
         transition={{ type: "spring", stiffness: 200, damping: 25 }}
-        className={`flex-1 p-6 overflow-y-auto ${PublicSans.className}`}>
+        className={`lg:flex-1 lg:p-6 lg:overflow-y-auto ${PublicSans.className}`}>
         <Overview />
       </motion.main>
     </div>
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -4,6 +4,12 @@ export default function Overview() {
   return (
     <>
       <h1 className='heading-xl'>Overview</h1>
+      <section>
+        <div className='body-m text-white bg-grey-900 p-250'>
+          <p>Current Balance</p>
+          <p>$4,835.00</p>
+        </div>
+      </section>
     </>
   );
 }
