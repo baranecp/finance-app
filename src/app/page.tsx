369e61f77149b0ddb7990050c566e283c0900200
baranecp <baranec.dev@gmail.com>
2025-10-02 15:42:09 +0200

    Refactor page layout with ClientWrapper for client-side logic and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,18 @@
-"use client";
-import { Public_Sans } from "next/font/google";
-import { useSidebarStore } from "@/store/useSidebarStore";
-import { motion } from "framer-motion";
-import { useMediaQuery } from "../hooks/useMediaQuery";
 import Navbar from "@/components/Navbar/Navbar";
 import Overview from "@/components/Overview";
 import Pots from "@/components/Pots";
 import Transactions from "@/components/Transactions";
-import "../styles/globals.css";
+import ClientWrapper from "./ClientWrapper";
 
-const PublicSans = Public_Sans({
-  weight: ["400", "700"],
-});
-
-export default function Home() {
-  const isLargeScreen = useMediaQuery("(min-width: 1024px)");
-  const { isOpen } = useSidebarStore();
+export default function Page() {
   return (
-    <div className='flex h-screen  bg-beige-100'>
+    <div className='flex h-screen bg-beige-100'>
       <Navbar />
-      <motion.main
-        animate={{ marginLeft: isLargeScreen ? (isOpen ? 40 : 40) : 0 }}
-        transition={{ type: "spring", stiffness: 200, damping: 25 }}
-        className={`md:px-10 md:mt-10 mt-6 px-4 pb-14 flex-1 overflow-y-auto ${PublicSans.className}`}>
+      <ClientWrapper>
         <Overview />
         <Pots />
         <Transactions />
-      </motion.main>
+      </ClientWrapper>
     </div>
   );
 }
